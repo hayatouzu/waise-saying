bdb884e3b9686d816aa2d3fd77ce436f18743d34
hayatouzu <kira.sri779@gmail.com>
2020-09-21 16:50:26 +0900

    update

---
+++ pages/category/_category.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div v-if="saying" class="container mx-auto text-center flex-col">
+      <p class="mt-10">名言：{{ saying.text }}</p>
+      <div v-if="!saying.image" class="h-64"></div>
+      <img
+        v-if="saying.image"
+        class="w-4/5 mx-auto mt-12"
+        :src="saying.image"
+      />
+      <p>作品：{{ saying.name }}</p>
+    </div>
+    <div class="fixed sayingButton">
+      <a href="#" @click.prevent="test">名言ボタン</a>
+      <a v-if="saying" target="_blanck" :href="tweet" class="ml-10"
+        >ツイートする</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    // paramsでアニメ、映画、偉人ymlを分けてデータを取ってきている「.caegory」で取ってきている
+    const saying = await $content('saying', params.category)
+    const sayingData = await saying.fetch()
+    return { sayingData: sayingData.sayings }
+  },
+  data() {
+    return {
+      saying: null,
+    }
+  },
+  computed: {
+    tweet() {
+      return `https://twitter.com/intent/tweet?text=${this.saying.text}&url=`
+    },
+  },
+  methods: {
+    test() {
+      const random = Math.round(Math.random() * (this.sayingData.length - 1))
+      this.saying = this.sayingData[random]
+      console.log(random)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.sayingButton {
+  transform: translateX(-50%);
+  left: 50%;
+  top: 70%;
+}
+</style>
